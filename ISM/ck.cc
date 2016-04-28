0a19e7652c835b35c799d1792d6020a78e21acc3
Tomas S. Grigera <tgrigera@iflysib.unlp.edu.ar>
2016-04-28 18:31:29 -0300

    Added ck (static correlation C(k))

--- ISM/ck.cc
+++ ISM/ck.cc
@@ -1,5 +1,5 @@
 /*
- * ck.cc -- Def2 in k space
+ * ck.cc -- Compute C(k) (analogous to the static structure factor)
  *
  *
  */
@@ -35,29 +35,29 @@ typedef glsim::ComplexFFT_gsl_2n  cFFT;
 
 class Ck {
 public:
-  Ck(double box_length[],double deltat_,int kn_,int kdir_);
+  Ck(double box_length[],int nk_,int kdir_,bool connected=false);
 
   Ck& push_config(glsim::OLconfiguration &conf);
   Ck& compute_Ck();
   const vcomplex& Ck_data() const {return Ck_;} 
 
 private:
-  int                               Npart,kn,kdir;
+  bool                              connected;
+  int                               nk,kdir,nsamp;
   double                            deltak_[3];
   std::vector<std::vector<double>>  k;
   double                            deltat;
-  // vcomplex                          jkx_,jky_,jkz_;
-  std::vector<vcomplex>             jkx_,jky_,jkz_;
   vcomplex                          Ck_;
   
-  vcomplex j_k(glsim::OLconfiguration &,double k[]);
+  void polarization(glsim::OLconfiguration& conf,double V[]);
+  void push_config_connected(glsim::OLconfiguration &conf);
+  void push_config_nc(glsim::OLconfiguration &conf);
 
   friend std::ostream& operator<<(std::ostream&,const Ck&); 
 } ;
 
-Ck::Ck(double box_length[],double deltat_,int kn_,int kdir_) :
-  deltat(deltat_),
-  kn(kn_), kdir(kdir_)
+Ck::Ck(double box_length[],int nk_,int kdir_,bool connected_) :
+  connected(connected_), nk(nk_), kdir(kdir_), nsamp(0)
 {
   // choose deltak
   deltak_[0]=2*M_PI/box_length[0];
@@ -73,87 +73,99 @@ Ck::Ck(double box_length[],double deltat_,int kn_,int kdir_) :
   for (int i=0; i<k.size(); ++i) {
     k[i].resize(3,0);
     if (kdir<0)
-      k[i][-kdir-1]=-deltak_[-kdir-1]*kn;
+      k[i][-kdir-1]=-deltak_[-kdir-1];
     else
-      k[i][kdir]=deltak_[kdir]*kn;
+      k[i][kdir]=deltak_[kdir];
     kdir--;
   }
-  jkx_.resize(k.size());
-  jky_.resize(k.size());
-  jkz_.resize(k.size());
+  Ck_.resize(nk,0);
 }
 
-vcomplex Ck::j_k(glsim::OLconfiguration &conf,double *k)
+void Ck::polarization(glsim::OLconfiguration& conf,double V[])
 {
-  std::vector<dcomplex> rk(3,dcomplex(0));
-
-  for (int i=0; i<conf.N; i++) {
+  memset(V,0,3*sizeof(double));
+  for (int i=0; i<conf.N; ++i) {
     double v0=sqrt(modsq(conf.v[i]));
-    double kr= k[0]*conf.r[i][0] + k[1]*conf.r[i][1] + k[2]*conf.r[i][2];
-    rk[0]+=conf.v[i][0]*exp(dcomplex(0,-1)*kr)/v0;
-    rk[1]+=conf.v[i][1]*exp(dcomplex(0,-1)*kr)/v0;
-    rk[2]+=conf.v[i][2]*exp(dcomplex(0,-1)*kr)/v0;
+    V[0]+=conf.v[i][0]/v0;
+    V[1]+=conf.v[i][1]/v0;
+    V[2]+=conf.v[i][2]/v0;
   }
-  return rk;
+  V[0]/=conf.N;
+  V[1]/=conf.N;
+  V[2]/=conf.N;
 }
 
 Ck& Ck::push_config(glsim::OLconfiguration &conf)
 {
-  Npart=conf.N;
-  for (int i=0; i<k.size(); ++i) {
-    std::vector<dcomplex> jk_;
-    jk_=j_k(conf,k[i].data());
-    jkx_[i].push_back(jk_[0]);
-    jky_[i].push_back(jk_[1]);
-    jkz_[i].push_back(jk_[2]);
-  }
-
+  if (connected) push_config_connected(conf);
+  else push_config_nc(conf);
   return *this;
 }
 
-Ck& Ck::compute_Ck()
+void Ck::push_config_nc(glsim::OLconfiguration &conf)
 {
-  dcomplex fac=1./dcomplex(k.size()*Npart,0);
-  int Cklen=jkx_[0].size()/2;
-  Ck_.clear();
-  Ck_.resize(Cklen,dcomplex(0,0));
-  cFFT FF(glsim::FFT::in_place);
+  dcomplex S;
+  dcomplex vkx,vky,vkz;
+  nsamp++;
+  dcomplex unsamp=1./dcomplex(nsamp,0);
+  for (int jk=0; jk<Ck_.size(); ++jk) {
+    S=0;
+    for (int i=0; i<k.size(); ++i) {
+      vkx=vky=vkz=0;
+      for (int n=0; n<conf.N; ++n) {
+	double v0=sqrt(modsq(conf.v[n]));
+	double kr= jk*k[i][0]*conf.r[n][0] + jk*k[i][1]*conf.r[n][1] + jk*k[i][2]*conf.r[n][2];
+	vkx+=conf.v[n][0]*exp(dcomplex(0,-1)*kr)/v0;
+	vky+=conf.v[n][1]*exp(dcomplex(0,-1)*kr)/v0;
+	vkz+=conf.v[n][2]*exp(dcomplex(0,-1)*kr)/v0;
+      }
+      S+=vkx*conj(vkx)+vky*conj(vky)+vkz*conj(vkz);
+    }
+    S/=(conf.N*k.size());  // Now S is Ck at fixed k, averaged over dirs
 
-  for (int i=0; i<k.size(); ++i) {
-    correlation_1d_tti_fft(jkx_[i],FF,Cklen);
-    for (int j=0; j<FF.tdata_rw().size(); j++) Ck_[j]+=fac*FF.tdatum(j);
-    correlation_1d_tti_fft(jky_[i],FF,Cklen);
-    for (int j=0; j<FF.tdata_rw().size(); j++) Ck_[j]+=fac*FF.tdatum(j);
-    correlation_1d_tti_fft(jkz_[i],FF,Cklen);
-    for (int j=0; j<FF.tdata_rw().size(); j++) Ck_[j]+=fac*FF.tdatum(j);
+    // Add to the running average of C(k)
+    dcomplex Q=S-Ck_[jk];
+    Ck_[jk]+=Q*unsamp;
   }
-
-  return *this;
 }
 
-struct optlst {
-public:
-  int  kn,kdir;
-  bool normalize;
-  std::vector<std::string> ifiles;
-} options;
+void Ck::push_config_connected(glsim::OLconfiguration &conf)
+{
+  dcomplex S;
+  dcomplex vkx,vky,vkz;
+  nsamp++;
+  dcomplex unsamp=1./dcomplex(nsamp,0);
 
+  double V[3];
+  polarization(conf,V);
+
+  for (int jk=0; jk<Ck_.size(); ++jk) {
+    S=0;
+    for (int i=0; i<k.size(); ++i) {
+      vkx=vky=vkz=0;
+      for (int n=0; n<conf.N; ++n) {
+	double v0=sqrt(modsq(conf.v[n]));
+	double kr= jk*k[i][0]*conf.r[n][0] + jk*k[i][1]*conf.r[n][1] + jk*k[i][2]*conf.r[n][2];
+	vkx+=(conf.v[n][0]/v0-V[0])*exp(dcomplex(0,-1)*kr);
+	vky+=(conf.v[n][1]/v0-V[1])*exp(dcomplex(0,-1)*kr);
+	vkz+=(conf.v[n][2]/v0-V[2])*exp(dcomplex(0,-1)*kr);
+      }
+      S+=vkx*conj(vkx)+vky*conj(vky)+vkz*conj(vkz);
+    }
+    S/=(conf.N*k.size());  // Now S is Ck at fixed k, averaged over dirs
+
+    // Add to the running average of C(k)
+    dcomplex Q=S-Ck_[jk];
+    Ck_[jk]+=Q*unsamp;
+  }
+}
 
 std::ostream& operator<<(std::ostream& o,const Ck& Ck_)
 {
-  o << "# time   Ck'   Ck''\n";
-  if (options.normalize) {
-    dcomplex fac=1./Ck_.Ck_[0];
-    for (int i=0; i<Ck_.Ck_.size(); i++) {
-      dcomplex cc=Ck_.Ck_[i]*fac;
-      o << i*Ck_.deltat << "  " << cc.real()
-	<< "  " << cc.imag() << '\n';
-    }
-  } else {
-    for (int i=0; i<Ck_.Ck_.size(); i++)
-      o << i*Ck_.deltat << "  " << Ck_.Ck_[i].real()
-	<< "  " << Ck_.Ck_[i].imag() << '\n';
-  }
+  o << "# k   Ck'   Ck''\n";
+  double dk = Ck_.kdir<3 && Ck_.kdir>=0 ? Ck_.deltak_[Ck_.kdir] : Ck_.deltak_[0];
+  for (int i=0; i<Ck_.Ck_.size(); i++)
+    o << i*dk << "  " << Ck_.Ck_[i].real() << "  " << Ck_.Ck_[i].imag() << '\n';
 }
 
 /******************************************************************************
@@ -161,6 +173,14 @@ std::ostream& operator<<(std::ostream& o,const Ck& Ck_)
  * Options and main
  *
  */
+
+struct optlst {
+public:
+  int  nk,kdir;
+  bool connect;
+  std::vector<std::string> ifiles;
+} options;
+
 
 class CLoptions : public glsim::UtilityCL {
 public:
@@ -171,25 +191,31 @@ public:
 CLoptions::CLoptions() : glsim::UtilityCL("ck")
 {
   hidden_command_line_options().add_options()
-    ("kn",po::value<int>(&options.kn)->required(),"wavevector")
+    ("nk",po::value<int>(&options.nk)->required(),"number of wavevectors to compute")
     ("ifiles",po::value<std::vector<std::string> >(&options.ifiles)->required(),"input files")
     ;
   command_line_options().add_options()
     ("kdirection,d",po::value<int>(&options.kdir)->required(),
      "direction of k: 0=x, 1=y, 2=z, 3=average of 0,1,2 (not good for noncubic boxes)")
-    ("normalize,N",po::bool_switch(&options.normalize)->default_value(false),
-     "Normalize correlation to 1 at t=0")
+    ("connect,C",po::bool_switch(&options.connect)->default_value(false),
+     "Computed connected C(k) (Roman style)")
      ;
 
-  positional_options().add("kn",1).add("ifiles",-1);
+  positional_options().add("nk",1).add("ifiles",-1);
 }
 
 void CLoptions::show_usage() const
 {
   std::cerr
-    << "usage: " << progname << "[options] kn ifile [ifile ....]\n\n"
-    << "Computes C(k) (def2) at wave vector kn (the desired multiple of Delta k).\n"
+    << "usage: " << progname << "[options] nk ifile [ifile ....]\n\n"
+    << "Computes C(k) for nk wavevectors kn (Delta k computed automatically).\n"
     << "Directions must be specified with option -d.\n"
+    << "This computes\n\n"
+    << "              C(k) = (1/N) \\sum_{ij} v_i v_i exp(i k r_{ij}) / v0^2\n\n"
+    << "or, if -C (connect) is given, the connected C(k), Roman style:\n\n"
+    << "              C_R(k) = (1/N) \\sum_{ij} \\delta v_i \\delta v_k exp(i k r_{ij}),\n\n"
+    << "where \\delta v_i = v_i/v_0 - (1/N) \\sum_i v_i/v_0.  The La Plata style \n"
+    << "connection can be obtained by computing C(k) and setting C(k=0) to 0.\n"
     << "\n"
     << " Options:\n";
   show_command_line_options(std::cerr);
@@ -201,25 +227,6 @@ void CLoptions::show_usage() const
  * main and deltat
  *
  */
-
-double get_deltat(glsim::H5_multi_file &ifs,glsim::OLconfiguration &conf)
-{
-  ifs.read();
-  double t0=conf.time;
-  ifs.read();
-  ifs.rewind();
-  return conf.time-t0;
-}
-
-void substract_cm(glsim::OLconfiguration &conf)
-{
-  std::vector<double> CM=conf.center_of_mass();
-  for (int i=0; i<conf.N; ++i) {
-    conf.r[i][0]-=CM[0];
-    conf.r[i][1]-=CM[1];
-    conf.r[i][2]-=CM[2];
-  }
-}
 
 void wmain(int argc,char *argv[])
 {
@@ -233,14 +240,10 @@ void wmain(int argc,char *argv[])
   if (options.kdir<0 || options.kdir>3)
     throw glsim::Runtime_error("kdir must be 0,1,3");
 
-  double deltat=get_deltat(ifs,conf);
-  Ck C(conf.box_length,deltat,options.kn,options.kdir);
+  Ck C(conf.box_length,options.nk,options.kdir,options.connect);
   while (ifs.read()) {
-    conf.unfold_coordinates();
-    substract_cm(conf);
     C.push_config(conf);
   }
-  C.compute_Ck();
   std::cout << C;
 }
 
